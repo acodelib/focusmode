b7f1cb5a7d72b6772072e370661c4ec212a95c1a
andrei-gurguta-veeam <andrei.gurguta@veeam.com>
2021-08-16 18:52:10 +0300

    oop format

--- tests.py
+++ tests.py
@@ -1,5 +1,5 @@
 import unittest
-import focusmode
+from focusmode import FocusMode
 import io
 import os
 from datetime import datetime 
@@ -11,78 +11,90 @@ class TestFocusMode(unittest.TestCase):
 #------------------------------------------------------------------------------------------------------------
 
     def test_isWorkingHours_Focus(self):        
-        target_timestamp = datetime.strptime("2021-08-11 14:12:53","%Y-%m-%d %H:%M:%S")   
-        check_result = focusmode.isWorkingHours(target_timestamp)
+        target_timestamp = datetime.strptime("2021-08-11 14:12:53","%Y-%m-%d %H:%M:%S")
+        service = FocusMode()   
+        check_result = service.isWorkingHours(target_timestamp)
         
         self.assertTrue(check_result)
         
     def test_isWorkingHours_Free_Weekeng(self):        
-        target_timestamp = datetime.strptime("2021-08-14 09:12:53","%Y-%m-%d %H:%M:%S")   
-        check_result = focusmode.isWorkingHours(target_timestamp)
+        target_timestamp = datetime.strptime("2021-08-14 09:12:53","%Y-%m-%d %H:%M:%S")
+        service = FocusMode()   
+        check_result = service.isWorkingHours(target_timestamp)
         
         self.assertFalse(check_result)  
     
     def test_isWorkingHours_Free_Morning(self):        
         target_timestamp = datetime.strptime("2021-08-12 08:55:53","%Y-%m-%d %H:%M:%S")   
-        check_result = focusmode.isWorkingHours(target_timestamp)
+        service = FocusMode()
+        check_result = service.isWorkingHours(target_timestamp)
         
         self.assertFalse(check_result)  
         
     def test_isWorkingHours_Free_Evening(self):        
-        target_timestamp = datetime.strptime("2021-08-12 19:00:00","%Y-%m-%d %H:%M:%S")   
-        check_result = focusmode.isWorkingHours(target_timestamp)
+        target_timestamp = datetime.strptime("2021-08-12 19:00:00","%Y-%m-%d %H:%M:%S")
+        service = FocusMode()   
+        check_result = service.isWorkingHours(target_timestamp)
         
         self.assertFalse(check_result)  
 #------------------------------------------------------------------------------------------------------          
     def test_computeFocusMode_Normal_Free(self):
         target_timestamp = datetime.strptime("2021-08-12 19:00:00","%Y-%m-%d %H:%M:%S")
         env_var = "Normal"
-        check_result = focusmode.computeFocusMode(env_var, target_timestamp)
+        service = FocusMode()
+        check_result = service.computeFocusMode(env_var, target_timestamp)
         
         self.assertEqual("Free", check_result)
         
     def test_computeFocusMode_Normal_Focus(self):
         target_timestamp = datetime.strptime("2021-08-12 10:00:00","%Y-%m-%d %H:%M:%S")
         env_var = "Normal"
-        check_result = focusmode.computeFocusMode(env_var, target_timestamp)
+        service = FocusMode()
+        check_result = service.computeFocusMode(env_var, target_timestamp)
         
         self.assertEqual("Focus", check_result)    
         
     def test_computeFocusMode_Focus(self):
         target_timestamp = datetime.strptime("2021-08-14 10:00:00","%Y-%m-%d %H:%M:%S")
         env_var = "Focus"
-        check_result = focusmode.computeFocusMode(env_var, target_timestamp)
+        service = FocusMode()
+        check_result = service.computeFocusMode(env_var, target_timestamp)
         
         self.assertEqual("Focus", check_result)    
         
     def test_computeFocusMode_Free(self):
         target_timestamp = datetime.strptime("2021-08-12 11:00:00","%Y-%m-%d %H:%M:%S")
         env_var = "Free"
-        check_result = focusmode.computeFocusMode(env_var, target_timestamp)
+        service = FocusMode()
+        check_result = service.computeFocusMode(env_var, target_timestamp)
         
         self.assertEqual("Free", check_result)
 #------------------------------------------------------------------------------------------------------
     def test_processEnvironVar_MissingEnvVar(self):
-        environ_dummy = {"TESTING":"NOTHING", "THETESTY":"SOMETHING", "OTHER":"OTHERS"}        
-        check_result = focusmode.processEnvironVar(environ_dummy)
+        environ_dummy = {"TESTING":"NOTHING", "THETESTY":"SOMETHING", "OTHER":"OTHERS"}
+        service = FocusMode()        
+        check_result = service.processEnvironVar(environ_dummy)
         
         self.assertEqual("Normal", check_result)
 
     def test_processEnvironVar_WrongValue(self):
-        environ_dummy = {"TESTING":"NOTHING", "THETESTY":"SOMETHING", "OTHER":"OTHERS", "FOCUSMODECONTROL":"Wrong"}        
-        check_result = focusmode.processEnvironVar(environ_dummy)
+        environ_dummy = {"TESTING":"NOTHING", "THETESTY":"SOMETHING", "OTHER":"OTHERS", "FOCUSMODECONTROL":"Wrong"}
+        service = FocusMode()        
+        check_result = service.processEnvironVar(environ_dummy)
         
         self.assertEqual("Normal", check_result)
         
     def test_processEnvironVar_Free(self):
-        environ_dummy = {"TESTING":"NOTHING", "THETESTY":"SOMETHING", "OTHER":"OTHERS", "FOCUSMODECONTROL":"Free"}        
-        check_result = focusmode.processEnvironVar(environ_dummy)
+        environ_dummy = {"TESTING":"NOTHING", "THETESTY":"SOMETHING", "OTHER":"OTHERS", "FOCUSMODECONTROL":"Free"}
+        service = FocusMode()        
+        check_result = service.processEnvironVar(environ_dummy)
         
         self.assertEqual("Free", check_result)
 #------------------------------------------------------------------------------------------------------        
     def test_getUrlFromString(self):
         test_str ="some stuff going won www.testingaddr.info this was a url"
-        check_result = focusmode.getUrlFromString(test_str)
+        service = FocusMode()
+        check_result = service.getUrlFromString(test_str)
         
         self.assertEqual("www.testingaddr.info", check_result)
 #------------------------------------------------------------------------------------------------------        
@@ -97,8 +109,9 @@ class TestFocusMode(unittest.TestCase):
 87.248.114.11 www.digi24.info
 10.101.122.104 www.piupiu.ro
 """
-        test_stream = io.StringIO(file_text)        
-        func_result = focusmode.checkMissingRedirectsFromFile(test_stream,url_list)      
+        test_stream = io.StringIO(file_text)
+        service = FocusMode()        
+        func_result = service.checkMissingRedirectsFromFile(test_stream,url_list)      
 
         self.assertTrue(set(expected_result) == set(func_result))       
 #------------------------------------------------------------------------------------------------------   
@@ -119,8 +132,9 @@ class TestFocusMode(unittest.TestCase):
 87.248.114.11 www.wikipedia.ro
 87.248.114.11 www.facebook.ro
 """
-        test_stream = io.StringIO(file_text)        
-        focusmode.appendRedirects(test_stream, url_list)                
+        test_stream = io.StringIO(file_text)
+        service = FocusMode()        
+        service.appendRedirects(test_stream, url_list)                
 
         self.assertEqual(expected_result, test_stream.getvalue())
                 
